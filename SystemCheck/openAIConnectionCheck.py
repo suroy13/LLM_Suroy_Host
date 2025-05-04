156b6b02aaae6afa45558c59c3b4b68ce37c6d8e
Subhankar Roy <subhankarr@outlook.com>
2025-05-04 16:06:11 -0400

    Initial Commit

---
+++ SystemCheck/openAIConnectionCheck.py
@@ -0,0 +1,52 @@
+# imports details
+
+import os
+import requests
+from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+from IPython.display import Markdown, display
+from openai import OpenAI
+
+
+load_dotenv(override=True)
+api_key = os.getenv('OPENAI_API_KEY')
+
+# Check the key
+
+if not api_key:
+    print("No API key was found - please head over to the troubleshooting notebook in this folder to identify & fix!")
+elif not api_key.startswith("sk-proj-"):
+    print("An API key was found, but it doesn't start sk-proj-; please check you're using the right key - see troubleshooting notebook")
+elif api_key.strip() != api_key:
+    print("An API key was found, but it looks like it might have space or tab characters at the start or end - please remove them - see troubleshooting notebook")
+else:
+    print("API key found and looks good so far!")
+	
+	
+openai = OpenAI()
+
+message = "Hello, GPT! This is my first ever message to you! Hi!"
+response = openai.chat.completions.create(model="gpt-4o-mini", messages=[{"role":"user", "content":message}])
+print(response.choices[0].message.content)
+
+# A class to represent a Webpage
+# If you're not familiar with Classes, check out the "Intermediate Python" notebook
+
+# Some websites need you to use proper headers when fetching them:
+# headers = {
+#  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+# }
+
+# class Website:
+
+#     def __init__(self, url):
+#         """
+#         Create this Website object from the given url using the BeautifulSoup library
+#         """
+#         self.url = url
+#         response = requests.get(url, headers=headers)
+#         soup = BeautifulSoup(response.content, 'html.parser')
+#         self.title = soup.title.string if soup.title else "No title found"
+#         for irrelevant in soup.body(["script", "style", "img", "input"]):
+#             irrelevant.decompose()
+#         self.text = soup.body.get_text(separator="\n", strip=True)
